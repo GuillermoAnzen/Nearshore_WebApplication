180d385a8f9bf8c51002395008ddfb9974e81ecb
Jorge Fernandez <jfernandezsistemas@gmail.com>
2017-05-09 12:43:01 -0500

    first controller

---
+++ app/scripts/controllers/logincontroller.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nearshoreWepapplicationApp.controller:LogincontrollerCtrl
+ * @description
+ * # LogincontrollerCtrl
+ * Controller of the nearshoreWepapplicationApp
+ */
+angular.module('nearshoreWepapplicationApp')
+  .controller('LogincontrollerCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
